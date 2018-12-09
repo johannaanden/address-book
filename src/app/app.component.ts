c3f1661b359960fa1699d96625e8a7da22ca73a5
johannaanden <johannaanden@hotmail.com>
2018-12-09 08:23:23 +0100

    Removes title from export class AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'address-book';
-
-  
-
   contact: any;
   contacts: any[] = [];
 
