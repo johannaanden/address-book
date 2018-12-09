d479721c9ccc3abcd9f2dbdb11441ad1c56edd71
johannaanden <johannaanden@hotmail.com>
2018-12-09 06:32:08 +0100

    Creates functions getField, getSubmitButton, cardCount and changes name on function getTitleText to  getParagraphText

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,19 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getTitleText() {
+  getParagraphText() {
     return element(by.css('app-root h1')).getText();
+  }
+
+  getField(name) {
+    return element(by.name(name));
+  }
+
+  getSubmitButton() {
+    return element(by.buttonText('Submit'));
+  }
+
+  cardCount() {
+    return element.all(by.css('.card')).count();
   }
 }
